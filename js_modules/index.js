a52ba14bdb6ba691c9d6bdf043f369cd47a3b5ac
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2026-06-08 20:48:33 +0530

    defined the two files

---
+++ js_modules/index.js
@@ -0,0 +1,7 @@
+/**  
+this file call functions from sum.js.... then prints the result
+since this index files uses some amount of code in sum.js file 
+so index.js file depends on sum.js file (sum.js -> index.js)
+first sum.js should be rendered and then index.js should render
+
+*/
---
+++ js_modules/sum.js
@@ -0,0 +1,3 @@
+// its a file for utility functions for working with mathematical functions
+
+const sum = (a,b) => a+b;
