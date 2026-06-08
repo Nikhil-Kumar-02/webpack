68249d68accacd18f4c9557c49b40b7fbf47c2c0
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2026-06-08 21:01:58 +0530

    initiated the webpack config file

--- js_modules/index.js
+++ js_modules/index.js
@@ -4,6 +4,11 @@ since this index files uses some amount of code in sum.js file
 so index.js file depends on sum.js file (sum.js -> index.js)
 first sum.js should be rendered and then index.js should render
 
+
+this file is the entry point for the project as it only imports the code
+and doesnt export and modules
+
+
 */
 
 const sum = require('./sum');
