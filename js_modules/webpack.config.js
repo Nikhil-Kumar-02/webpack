e5e480b1b93c37e7b1aff6022db3bd17e0f2533d
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2026-06-09 22:35:50 +0530

    configured webpack and created a build in production and development environment

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -1,10 +1,25 @@
 const path = require('path')
 
 const config = {
+  mode: 'production',
   entry : './src/index.js',
   output : {
     path : path.resolve(__dirname,'build'),
     filename : 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
 
